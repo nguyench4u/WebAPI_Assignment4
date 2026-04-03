5843d4ed5eedb74c4ab528aca453a5bc7ebf90a0
Chau Nguyen on Windows <nguyench4u@gmail.com>
2026-04-03 15:08:50 -0600

    Step 1.5: Copy Movies Schema from Assignment 3

--- Movies.js
+++ Movies.js
@@ -4,8 +4,23 @@ var Schema = mongoose.Schema;
 mongoose.connect(process.env.DB);
 
 // Movie schema
+// Taken exactly from Assignment 3 
 var MovieSchema = new Schema({
-
+    title: { type: String, required: true, index: true },
+    releaseDate: { type: Number, required: true, min: [1900, 'Must be greater than 1899'] },
+    genre: {
+        type: String,
+        required: true,
+        enum: ['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'],
+    },
+    actors: {
+        type: [{
+            actorName: { type: String, required: true },
+            characterName: { type: String, required: true },
+        }],
+        required: true,
+        validate: [arr => arr.length >= 3, 'Must have at least 3 actors'],
+    },
 });
 
 // return the model
